91973c2df88c54af76e471bc042a6c9ed0a5cffa
Daniel <brikbusters@gmail.com>
2022-01-06 19:02:40 -0500

    add downloader

--- Makefile
+++ Makefile
@@ -1,6 +1,9 @@
 .PHONY: run
-run: images11.html
+run: images11.html images
 	python3 main.py
+
+images:
+	mkdir images
 
 images11.html:
 	wget -4 https://www.hq.nasa.gov/alsj/a11/images11.html
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import re
+import re, os
 
 fp = open("images11.html", "rb")
 c = fp.read()
@@ -7,8 +7,7 @@ c = str(c)
 
 find = re.findall(r"AS11-([0-9]+)-([0-9]+)HR\.jpg", c)
 for img in find:
-    print(
+    url = (
         "https://www.hq.nasa.gov/alsj/a11/" + 
         "AS11-" + img[0] + "-" + img[1] + "HR.jpg")
-
-print("Total", str(len(find)), "images")
+    os.system("cd images; wget  " + url)
